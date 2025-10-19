e77e6eb76fff041d123b4da193e95d4792a14153
Nueledu <162829880+Nueledu@users.noreply.github.com>
2025-10-19 09:00:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* =============================== typing animation ======================================= */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer"],
+    strings:["","Web Designer","Web Developer","Real estate","E-commerce","Landing page","Crypto currency","And lots more"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
